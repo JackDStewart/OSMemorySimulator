098ec1bd382e4bfe26c453765298b1c3281a18c8
Owen Marcione <omarcione@Owens-MacBook-Air-4.local>
2026-02-23 00:07:37 -0800

    implemented OPT, made fixes to fifo, and general debug/fixes. Need to test

--- memSim.c
+++ memSim.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
+#include <limits.h>
 
 // Constants
 #define PAGE_SIZE 256   // Size of a page in bytes
@@ -38,12 +39,18 @@ static int *frame_to_page; // frame -> page
 static int *fifo_queue = NULL;
 static int  fifo_head = 0;
 static int  fifo_tail = 0;
+int num_requests; // total number of page requests, len of page_requests array
+static unsigned int *page_requests; // For OPT: Stores order of page requests
+
+
+int time = 0; // put in last_used to keep track of time when frame accessed, increments each iteration of while loop
 
 // Function prototypes
 static void parse_arguments(int argc, char *argv[]);
 static void print_output(int addr_cnt, int page_fault_cnt, int tlb_hit_cnt, int tlb_miss_cnt);
 int in_tlb(unsigned int page);
 int pick_victim(int *last_used);
+
 
 static void fifo_push(int frame)
 {
@@ -110,12 +117,12 @@ static void print_output(int addr_cnt, int page_fault_cnt, int tlb_hit_cnt, int 
     if ((tlb_hit_cnt + tlb_miss_cnt) > 0) {
         tlb_hit_rate = (double)tlb_hit_cnt / (tlb_hit_cnt + tlb_miss_cnt);
     }
-    printf("Number of Translated Addresses = %x\n", addr_cnt);
+    printf("Number of Translated Addresses = %d\n", addr_cnt);
     printf("Page Faults = %d\n", page_fault_cnt);
-    printf("Page Fault Rate = %.2f%%\n", (double)page_fault_cnt / addr_cnt);
+    printf("Page Fault Rate = %.3f\n", (double)page_fault_cnt / addr_cnt);
     printf("TLB Hits = %d\n", tlb_hit_cnt);
     printf("TLB Misses = %d\n", tlb_miss_cnt);
-    printf("TLB Hit Rate = %.2f%%\n", tlb_hit_rate * 100);
+    printf("TLB Hit Rate = %.3f\n", tlb_hit_rate * 100);
 }
 
 int in_tlb(unsigned int page){ // returns idx in tlb, -1 if not present
@@ -137,7 +144,7 @@ int pick_victim(int *last_used) {
 
     case LRU: {
         int lru_frame = 0;
-        int lru_time = __INT_MAX__; // initialize to max int so any real time will be smaller
+        int lru_time = INT_MAX; // initialize to max int so any real time will be smaller
         for (int i = 0; i < num_frames; i++) {
             if (last_used[i] < lru_time) {
                 lru_time = last_used[i];
@@ -147,8 +154,28 @@ int pick_victim(int *last_used) {
         return lru_frame;
     }
 
-    case OPT:
-
+    case OPT:;
+        int victim_frame = 0;
+        int victim_idx = -1; // anything else will be larger
+        for (int i = 0; i < num_frames; i++) { // for each current frame
+            int p =  frame_to_page[i];
+            int j = time;
+            int found = 0;
+            // scan all future frames, find which of current frames is farthest 
+            while (j < num_requests) { 
+                if ((int)page_requests[j] == p) {
+                    if (j > victim_idx) { // if difference is bigger
+                        victim_idx = j;
+                        victim_frame = i;
+                    }
+                    found = 1;
+                    break;
+                }
+                j++;
+            }
+            if (!found) return i;
+        }
+        return victim_frame;
         break;
     }
     return 0;
@@ -158,7 +185,6 @@ int pick_victim(int *last_used) {
 // FRAMES: number of frames in the memory FRAMES (integer <= 256 and > 0)
 // PRA: “FIFO” or “LRU” or “OPT”
 int main(int argc, char *argv[]) {
-
     parse_arguments(argc, argv);
 
     // allocate physical memory
@@ -172,21 +198,36 @@ int main(int argc, char *argv[]) {
     frame_to_page = malloc(num_frames * sizeof(int));
     if (!frame_to_page) {
         perror("Error allocating frame to page");
+        free(physical_mem);
         exit(EXIT_FAILURE);
     }
     for (int i = 0; i < num_frames; i++)
         frame_to_page[i] = -1;
 
-    
+    fifo_queue = malloc(num_frames * FRAME_SIZE);
+    if (!fifo_queue) {
+        perror("Error allocating FIFO queue");
+        free(physical_mem);
+        free(frame_to_page);
+        exit(EXIT_FAILURE);
+    }
+    fifo_head = fifo_tail = 0;
+
     FILE *input_file = fopen(input_path, "r");
     if (!input_file) {
         perror("Error opening input file");
+        free(physical_mem);
+        free(frame_to_page);
+        free(fifo_queue);
         exit(EXIT_FAILURE);
     }
 
     FILE *backing_store = fopen("BACKING_STORE.bin", "rb");
     if (!backing_store) {
         perror("Error opening backing store");
+        free(physical_mem);
+        free(frame_to_page);
+        free(fifo_queue);
         exit(EXIT_FAILURE);
     }
 
@@ -200,17 +241,51 @@ int main(int argc, char *argv[]) {
 
     int tlb_idx = 0; // tlb FIFO index, increment when used, then %= 16
 
-    const size_t line_len = 4; // 32 bits -> 4 bytes
-    char line[line_len];
+    const size_t line_len = 32; // 32 bytes
+    char line[line_len]; // buffer to hold line
+    num_requests = 0; // total numvber of page requests, used for OPT
 
     int last_used[num_frames];
     for (int i = 0; i < num_frames; i++){
         last_used[i] = 0;
     }
 
-    int time = 0; // put in last_used to keep track of time when frame accessed, increments each iteration of while loop
+    if (algorithm == OPT) { // get sequence of requests only once so we can limit reads
+        //get len of input file to malloc page_requests
+        int cap = 4;
+        page_requests = malloc(cap * sizeof(unsigned int));
+        if (!page_requests) {
+            perror("Error allocating page request array for OPT");
+            free(physical_mem);
+            free(frame_to_page);
+            free(fifo_queue);
+            exit(EXIT_FAILURE);
+        }
+        while (fgets(line, line_len, input_file)){
+            if (num_requests == cap) {
+                cap *=2;
+                unsigned int *tmp = realloc(page_requests, cap * sizeof(*page_requests));
+                if (!tmp){
+                    perror("Error reallocating page request array for OPT");
+                    free(page_requests);
+                    free(physical_mem);
+                    free(frame_to_page);
+                    free(fifo_queue);
+                    exit(EXIT_FAILURE);
+                }
+                page_requests = tmp;
+            }
+            page_requests[num_requests] = (atoi(line) >> 8) & 0xFF;
+            num_requests++;
+        }
+        // try to shrink to i only
+        unsigned int *tmp = realloc(page_requests, num_requests * sizeof(*page_requests));
+        if (tmp) page_requests = tmp;
+        rewind(input_file); // go back to beginning
+    }
 
     while(fgets(line, line_len, input_file)) {
+        line[strcspn(line, "\n")] = '\0';
         addr_cnt++;
         // extract address
         int virtual_addr = atoi(line); // convert ascii to int, ignore upper 16 bits
@@ -245,7 +320,11 @@ int main(int argc, char *argv[]) {
                     size_t n = fread(physical_mem + mem_offset, 1, 256, backing_store);
                     if (n != FRAME_SIZE) {
                         perror("Error reading from backing store");
-                        exit(1);
+                        free(physical_mem);
+                        free(frame_to_page);
+                        free(page_requests);
+                        free(fifo_queue);
+                        exit(EXIT_FAILURE);
                     }
                     frame_to_page[frame] = page; // used so we can invalidate the page if we evict its frame
                     empty_frames--;
@@ -256,6 +335,8 @@ int main(int argc, char *argv[]) {
                     int victim_frame = pick_victim(last_used);
                     int victim_page = frame_to_page[victim_frame];
                     page_table[victim_page].present = 0; // invalidate old page
+                    tlb_entry = in_tlb(victim_page);
+                    tlb[tlb_entry].valid = 0;
 
                     long file_offset = (long)page * 256;
                     long mem_offset  = (long)victim_frame * 256;
@@ -263,7 +344,11 @@ int main(int argc, char *argv[]) {
                     size_t n = fread(physical_mem + mem_offset, 1, 256, backing_store);
                     if (n != FRAME_SIZE) {
                         perror("Error reading from backing store");
-                        exit(1);
+                        free(physical_mem);
+                        free(frame_to_page);
+                        free(page_requests);
+                        free(fifo_queue);
+                        exit(EXIT_FAILURE);
                     }
 
                     frame_to_page[victim_frame] = page; // update frame to page mapping
@@ -271,7 +356,8 @@ int main(int argc, char *argv[]) {
                 }
                 page_entry->present = 1;
                 page_entry->frame_number = frame;
-                fifo_push(frame); // add new frame to FIFO queue if page fault occurs
+                if (algorithm == FIFO) 
+                    fifo_push(frame); // add new frame to FIFO queue if page fault occurs
             }
 
             last_used[frame] = time;    // update last used for LRU
@@ -287,9 +373,9 @@ int main(int argc, char *argv[]) {
         int phys_addr = frame * FRAME_SIZE;
         unsigned char frame_data[FRAME_SIZE];
         memcpy(&frame_data, &physical_mem[phys_addr], FRAME_SIZE); //get whole frame
-        int value = frame_data[offset]; //get specified byte
+        int value = (signed char)frame_data[offset]; //get specified byte
         // print virtual address, value of byte, frame number
-        printf("%s, %x, %u, ", line, value, frame);
+        printf("%s, %d, %u, ", line, value, frame);
         // print entire frame
         for (int i = 0; i < FRAME_SIZE; i++) {
             printf("%02x", frame_data[i]);
@@ -303,5 +389,7 @@ int main(int argc, char *argv[]) {
 
     free(physical_mem);
     free(frame_to_page);
+    free(page_requests);
+    free(fifo_queue);
     return 0;
 }
